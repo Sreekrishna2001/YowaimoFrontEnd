932256f54a95b210ee8ab69601c19826f94b0100
Radhakrishna <7krishna3radha@gmail.com>
2022-06-27 02:58:07 +0530

    Modified SearchPage

--- src/components/SearchAnime/SearchAnime.js
+++ src/components/SearchAnime/SearchAnime.js
@@ -18,6 +18,7 @@ export default function SearchAnime() {
     return (
         <>
             <div className="text-center border-bottom mb-2 bg-dark"><h3 className="text-white">Search Results</h3></div>
+            <div className="text-center"><h4>{searchData.length === 0 ? "No Results Found" : ""}</h4></div>
             <div className='homeLayout'>
                 {searchData.map(anime => <Link to={`../../anime/${anime.anime}`}><AnimeCard key={anime.anime} cover={anime.thumbnail} name={anime.anime} /></Link>)}
             </div>
