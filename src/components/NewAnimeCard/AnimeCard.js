aa784f7f53db6ad025baa2338128ba853c726a77
Sree Krishna Maremanda <maremandasreekrishna@gmail.com>
2022-06-30 01:42:27 +0530

    added proxy for anime cards cover

--- src/components/NewAnimeCard/AnimeCard.js
+++ src/components/NewAnimeCard/AnimeCard.js
@@ -4,7 +4,7 @@ import './AnimeCard.css'
 export default function AnimeCard({ cover, name }) {
     return (
         <div className="card">
-            <img className="card-img-top" src={cover} alt={name} />
+            <img className="card-img-top" src={'https://yowaimoproxy.herokuapp.com/?url='+cover} alt={name} />
             <div className="card-body">
                 <p className="card-text">{name}</p>
             </div>
