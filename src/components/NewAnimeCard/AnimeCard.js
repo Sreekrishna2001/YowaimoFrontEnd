dd5c6d2b2d67620007598e05b45a8a85aab20720
Sree Krishna Maremanda <maremandasreekrishna@gmail.com>
2022-07-05 17:24:47 +0530

    Added search for kdramas

--- src/components/NewAnimeCard/AnimeCard.js
+++ src/components/NewAnimeCard/AnimeCard.js
@@ -4,7 +4,7 @@ import './AnimeCard.css'
 export default function AnimeCard({ cover, name }) {
     return (
         <div className="card">
-            <img className="card-img-top" src={'https://corsproxy.io/?'+cover} alt={name} />
+            <img className="card-img-top" src={'https://corsproxy.io/?'+encodeURIComponent(cover)} alt={name} />
             <div className="card-body">
                 <p className="card-text">{name}</p>
             </div>
--- src/components/SearchAnime/SearchAnime.js
+++ src/components/SearchAnime/SearchAnime.js
@@ -8,10 +8,12 @@ import '../Home/Home.css'
 export default function SearchAnime() {
     const { animeName } = useParams();
     const [searchData, setSearchData] = useState([])
+    const [ksearchData,setKsearchData] = useState([])
 
     useEffect(() => {
         const fetchData = async () => {
             setSearchData(await searchRequest('GET', `search/${animeName}`))
+            setKsearchData(await searchRequest('GET',`kdrama/search/${animeName}`))
         }
         fetchData()
         // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -29,10 +31,15 @@ export default function SearchAnime() {
     return (
         <>
             <PageLoader />
-            <div className="text-center border-bottom mb-2 bg-dark"><h3 className="text-white">Search Results</h3></div>
+            <div className="text-center border-bottom mb-2 bg-dark"><h3 className="text-white">Anime Search Results</h3></div>
             <div className="text-center"><h4>{searchData.length === 0 ? "No Results Found" : ""}</h4></div>
             <div className='row m-0'>
                 {searchData.map(anime => <div className="col-md-3 col-lg-2 col-6 mb-1" key={anime.anime}> <Link to={`../../anime/${anime.anime}`}><AnimeCard key={anime.anime} cover={anime.thumbnail} name={anime.anime} /></Link> </div>)}
+            </div>
+            <div className="text-center border-bottom mb-2 bg-dark"><h3 className="text-white">Kdrama Search Results</h3></div>
+            <div className="text-center"><h4>{searchData.length === 0 ? "No Results Found" : ""}</h4></div>
+            <div className='row m-0'>
+                {ksearchData.map(anime => <div className="col-md-3 col-lg-2 col-6 mb-1" key={anime.drama}> <Link to={`../../kdrama/${anime.drama}`}><AnimeCard key={anime.drama} cover={anime.cover} name={anime.drama} /></Link> </div>)}
             </div>
         </>
     )
