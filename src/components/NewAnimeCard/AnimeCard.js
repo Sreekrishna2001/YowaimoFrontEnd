7ef04e8050fdabcc78f56ef6999d8de7a3289484
Sree Krishna Maremanda <maremandasreekrishna@gmail.com>
2022-08-18 00:23:29 +0530

    Feat 🎸: Achieved responsiveness

--- src/components/NewAnimeCard/AnimeCard.js
+++ src/components/NewAnimeCard/AnimeCard.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function AnimeCard({ cover, name }) {
     return (
-        <div className="w-56 h-80">
-                <img className="rounded-xl w-56 h-80 hover:blur-sm hover:cursor-pointer hover:h-72 hover:w-52 hover:transition duration-200" src={'https://corsproxy.io/?'+encodeURIComponent(cover.replaceAll(' ','%20'))} alt={name}  />
+        <div className="lg:w-56 lg:h-80 w-24 h-36">
+                <img className="rounded-xl lg:hover:blur-sm lg:w-56 lg:h-80 w-24 h-36 object-cover hover:cursor-pointer lg:hover:h-72 lg:hover:w-52 hover:transition duration-100" src={'https://corsproxy.io/?'+encodeURIComponent(cover.replaceAll(' ','%20'))} alt={name}  />
             <p className="truncate ..." title={name}>
                 {name}
             </p>
