1be7db7de48856f4455de00477eaf7b868de0f03
Sree Krishna Maremanda <maremandasreekrishna@gmail.com>
2022-09-02 12:28:48 +0530

    feat 🎸:ditched heroku for good

--- src/NetworkCalls/api.js
+++ src/NetworkCalls/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const BASE_URL_LOCAL = 'http://localhost:5000/'s
-export const BASE_URL_DEPLOYED = 'https://saikou.herokuapp.com/'
+export const BASE_URL_DEPLOYED = 'http://3.93.165.26:5000/'
 
 export const makeRequest = async (method, uri, params = {}) => {
     return await (await axios.get(BASE_URL_DEPLOYED + uri)).data
